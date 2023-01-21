c7cbc4d0821313c78550002fb84e491f8e543a5a
Alex Granatowicz <alex.g@oneupsales.co.uk>
2023-01-21 16:31:28 +0000

    Rewriting lib

--- stepper_test.py
+++ stepper_test.py
@@ -1,29 +1,47 @@
-import RPi.GPIO as GPIO
+class Status: 
+    UP = 'up'
+    DOWN = 'down'
 
-# import the library
-from RpiMotorLib import RpiMotorLib
-    
-GpioPins = [18, 23, 24, 25]
+    @staticmethod
+    def get():
+        with open('flag.txt', 'r') as file:
+            data = file.readlines()
+        
+        if len(data) < 1:
+            data.append('')
+            
+        return data[0]
 
-# Declare an named instance of class pass a name and motor type
-mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
+    @staticmethod
+    def set(status):
+        if status not in [UP, DOWN]:
+            raise Exeption("Only 'up' and 'down' are valid statuses")
+        with open('flag.txt', 'w') as file:
+            file.writelines(status)
 
-flagUp = True
+class Flag:
+    def __init__():
+        from RpiMotorLib import RpiMotorLib
+            
+        GpioPins = [18, 23, 24, 25]
 
-with open('flag.txt', 'r') as file:
-    data = file.readlines()
-    
-if len(data) < 1:
-    data.append('')
-    
-flagUp = data[0] == 'up'
-data[0] = 'down' if flagUp else 'up'
+        # Declare an named instance of class pass a name and motor type
+        self.motor = RpiMotorLib.BYJMotor("FlagMotor", "28BYJ")
+        
 
-with open('flag.txt', 'w') as file:
-    file.writelines(data)
-    
-# call the function pass the parameters
-mymotortest.motor_run(GpioPins , 0.0018, 128, not flagUp, False, "full", .005)
+    def __del__(self):
+        import RPi.GPIO as GPIO
+        # good practise to cleanup GPIO at some point before exit
+        GPIO.cleanup()
 
-# good practise to cleanup GPIO at some point before exit
-GPIO.cleanup()
+    # Number of steps in a full revolution of the motor
+    REVOLUTION = 512
+    def move(clockwise = True, dist = 0.25):
+        self.motor.motor_run(GpioPins , 0.0018, int(dist * REVOLUTION), not clockwise, False, "full", .005)
+
+flag = Flag()
+status = Status.get()
+
+flag.move(status == Status.UP)
+Status.set(Status.UP if status == Status.DOWN else Status.DOWN)
+del flag
